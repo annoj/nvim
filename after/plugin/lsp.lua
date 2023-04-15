b5d830cbf2690a7ff9d7946e141f1cacc9fb8fce
Jona Heitzer <jona.heitzer@secuinfra.com>
2023-04-16 01:05:08 +0200

    Customize LSP keybindings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,12 +1,30 @@
 local lsp = require('lsp-zero').preset({})
 
 lsp.ensure_installed({
-	'clangd',
-	'pylsp',
+    'clangd',
+    'pylsp',
+})
+
+local cmp = require('cmp')
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+lsp.setup_nvim_cmp({
+    mapping = cmp_mappings
 })
 
 lsp.on_attach(function(client, bufnr)
-	lsp.default_keymaps({buffer = bufnr})
+    lsp.default_keymaps({ buffer = bufnr })
+
+    local opts = { buffer = bufnr, remap = false }
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
 end)
 
 -- (Optional) Configure lua language server for neovim
