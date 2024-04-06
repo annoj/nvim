b02b84bec55149667d9e9ccc9ce2e5dfd52a6b27
Jona Heitzer <jona.heitzer@wa5p.xyz>
2024-04-06 22:10:49 +0200

    Automatically bootstrap packer

--- lua/jona/packer.lua
+++ lua/jona/packer.lua
@@ -1,71 +1,88 @@
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+		vim.cmd [[packadd packer.nvim]]
+		return true
+	end
+	return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
 
-    use {
-        'nvim-telescope/telescope.nvim',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
+	use {
+		'nvim-telescope/telescope.nvim',
+		requires = { { 'nvim-lua/plenary.nvim' } }
+	}
 
-    use({
-        'rose-pine/neovim',
-        as = 'rose-pine',
-        config = function()
-            vim.cmd('colorscheme rose-pine')
-        end
-    })
+	use({
+		'rose-pine/neovim',
+		as = 'rose-pine',
+		config = function()
+			vim.cmd('colorscheme rose-pine')
+		end
+	})
 
-    use('nvim-treesitter/nvim-treesitter', { run = ':TSupdate' })
-    use('nvim-treesitter/playground')
-    use('theprimeagen/harpoon')
-    use('mbbill/undotree')
-    use('tpope/vim-fugitive')
+	use('nvim-treesitter/nvim-treesitter', { run = ':TSupdate' })
+	use('nvim-treesitter/playground')
+	use('theprimeagen/harpoon')
+	use('mbbill/undotree')
+	use('tpope/vim-fugitive')
 
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        requires = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' }, -- Required
-            {
-                              -- Optional
-                'williamboman/mason.nvim',
-                run = function()
-                    pcall(vim.cmd, 'MasonUpdate')
-                end,
-            },
-            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		requires = {
+			-- LSP Support
+			{ 'neovim/nvim-lspconfig' }, -- Required
+			{
+				-- Optional
+				'williamboman/mason.nvim',
+				run = function()
+					pcall(vim.cmd, 'MasonUpdate')
+				end,
+			},
+			{ 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' }, -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'hrsh7th/cmp-nvim-lua' }, -- Required
-            { 'L3MON4D3/LuaSnip' }, -- Required
-            { 'hrsh7th/cmp-path' }
-        }
-    }
+			-- Autocompletion
+			{ 'hrsh7th/nvim-cmp' }, -- Required
+			{ 'hrsh7th/cmp-nvim-lsp' }, -- Required
+			{ 'hrsh7th/cmp-nvim-lua' }, -- Required
+			{ 'L3MON4D3/LuaSnip' }, -- Required
+			{ 'hrsh7th/cmp-path' }
+		}
+	}
 
-    use {
-        "FotiadisM/tabset.nvim",
-        config = function()
-            require("tabset").setup()
-        end
-    }
+	use {
+		"FotiadisM/tabset.nvim",
+		config = function()
+			require("tabset").setup()
+		end
+	}
 
-    use {
-        's1n7ax/nvim-terminal',
-        config = function()
-            vim.o.hidden = true
-            require('nvim-terminal').setup()
-        end,
-    }
+	use {
+		's1n7ax/nvim-terminal',
+		config = function()
+			vim.o.hidden = true
+			require('nvim-terminal').setup()
+		end,
+	}
 
-    -- Debugger stuff
-    use 'mfussenegger/nvim-dap'
-    use 'rcarriga/nvim-dap-ui'
-    use 'theHamsta/nvim-dap-virtual-text'
-    use 'nvim-telescope/telescope-dap.nvim'
-    use 'mortepau/codicons.nvim'
+	-- Debugger stuff
+	use 'mfussenegger/nvim-dap'
+	use 'rcarriga/nvim-dap-ui'
+	use 'theHamsta/nvim-dap-virtual-text'
+	use 'nvim-telescope/telescope-dap.nvim'
+	use 'mortepau/codicons.nvim'
 
-    use 'tjdevries/stackmap.nvim'
+	use 'tjdevries/stackmap.nvim'
+
+	if packer_bootstrap then
+		require('packer').sync()
+	end
 end)
